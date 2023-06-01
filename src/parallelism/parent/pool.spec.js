5420e2b497eddc8922e79a549935b6be58c39a82
Àlex Serra <bounteous17@protonmail.com>
2023-06-01 17:00:14 +0200

    test: muliple thread script execution

--- src/parallelism/parent/pool.spec.js
+++ src/parallelism/parent/pool.spec.js
@@ -12,21 +12,34 @@ describe('main worker', () => {
                 getTestsList: jest.fn(() => ['script_1.js', 'script_2.js'])
             },
             cpu: {
-                getParallelismAmount: jest.fn(() => 1)
+                getParallelismAmount: (amount) => jest.fn(() => amount)
             }
         }
     };
 
     beforeEach(() => {
         jest.mock('../../os/fs', () => mocks.os.fs);
-        jest.mock('../../os/cpu', () => mocks.os.cpu);
         jest.mock('./worker', () => mocks.parallelism.parent.worker);
     });
 
-    it('pool creation', () => {
+    it('pool creation single thread', () => {
+        jest.mock('../../os/cpu', () => ({
+            getParallelismAmount: mocks.os.cpu.getParallelismAmount(1)
+        }));
         const { createTestsPools } = require('./pool');
         createTestsPools();
         expect(mocks.parallelism.parent.worker.create).toHaveBeenCalledTimes(2);
+        expect(mocks.parallelism.parent.worker.create).toHaveBeenCalledWith('script_1.js');
+        expect(mocks.parallelism.parent.worker.create).toHaveBeenCalledWith('script_2.js');
+    });
+
+    it('pool creation multiple threads', () => {
+        jest.mock('../../os/cpu', () => ({
+            getParallelismAmount: mocks.os.cpu.getParallelismAmount(2)
+        }));
+        const { createTestsPools } = require('./pool');
+        createTestsPools();
+        expect(mocks.parallelism.parent.worker.create).toHaveBeenCalledTimes(4);
         expect(mocks.parallelism.parent.worker.create).toHaveBeenCalledWith('script_1.js');
         expect(mocks.parallelism.parent.worker.create).toHaveBeenCalledWith('script_2.js');
     });
