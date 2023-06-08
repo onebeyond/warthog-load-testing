ca22d0c7cdc05358f3a7c4a4d69b3a6a37a644c0
Àlex Serra <bounteous17@protonmail.com>
2023-06-08 16:32:32 +0200

    fix: non needed eslint rule skip

--- src/parallelism/threads/exec.js
+++ src/parallelism/threads/exec.js
@@ -9,7 +9,6 @@ async function executeChild() {
         workerData.iterations = iterations;
     });
 
-    // eslint-disable-next-line import/no-dynamic-require, global-require
     const testLifecycleValidator = validateTestLifecycle.byTestPath(workerData.path);
     const { setup, test } = {
         setup: testLifecycleValidator.setup(),
