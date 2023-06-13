ad0df3764e7e6594dc789a6aad3c0a89eaf5b8a8
Àlex Serra <bounteous17@protonmail.com>
2023-06-13 13:06:59 +0200

    test: notify parent lifecycle

--- src/parallelism/threads/process-communication.spec.js
+++ src/parallelism/threads/process-communication.spec.js
@@ -7,7 +7,7 @@ describe('parent thread communications', () => {
         }
     }
 
-    it('subscribe child', () => {
+    beforeEach(() => {
         jest.mock('node:worker_threads', () => {
             return {
                 workerData: {
@@ -16,12 +16,30 @@ describe('parent thread communications', () => {
                 parentPort: new MockParentPortEventEmitter()
             };
         });
+    });
 
+    it('subscribe child', () => {
         const { subscribeParent } = require('./process-communication');
         subscribeParent();
 
         const { parentPort, workerData } = require('node:worker_threads');
         parentPort.postMessage({ iterations: 2 });
         expect(workerData.iterations).toBe(2);
+    });
+
+    describe('notify parent', () => {
+        describe('lifecycle', () => {
+            it('setup', () => {
+                const { notify } = require('./process-communication');
+                const { parentPort } = require('node:worker_threads');
+
+                const subscribeMessageMock = jest.fn(() => {});
+                parentPort.on('message', subscribeMessageMock);
+
+                expect(subscribeMessageMock).toHaveBeenCalledTimes(0);
+                notify.finished.lifecycle.setup();
+                expect(subscribeMessageMock).toHaveBeenCalledTimes(1);
+            });
+        });
     });
 });
