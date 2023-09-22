7323022d47f61b878e36f81f4ff413ca03bf12e3
Àlex Serra <bounteous17@protonmail.com>
2023-09-22 13:01:14 +0200

    test: avoid jest force exit

--- jest.config.js
+++ jest.config.js
@@ -121,7 +121,7 @@ module.exports = {
     // restoreMocks: false,
 
     // The root directory that Jest should scan for tests and modules within
-    rootDir: 'src',
+    rootDir: 'src'
 
     // A list of paths to directories that Jest should use to search for files in
     // roots: [
@@ -192,6 +192,4 @@ module.exports = {
 
     // Whether to use watchman for file crawling
     // watchman: true,
-
-    forceExit: true
 };
